38f97ae96c6ec0784d9b2d18a458b8928132de52
CamBamBoozle <82921738+CamBamBoozle@users.noreply.github.com>
2021-07-29 21:17:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+        }
+    if (principal > 0) {
+            document.getElementById("result").innerText=(
+                "If you deposit " + principal + ",\nat an interest rate of " + rate + ".\nYou will recieve an amount of " + interest + ",\nin the year " + year
+                );
+        }
+
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}    

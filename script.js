def0c8e713db6baf68822a3211355595d716f16a
CamBamBoozle <82921738+CamBamBoozle@users.noreply.github.com>
2021-08-01 17:14:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 function compute()
-{
+    {
     var principle = document.getElementById("principle").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
+    var interest = principle * years * rate /100;
 
     var year = new Date().getFullYear()+parseInt(years);
-    
 
     if (principle > 0) {
-            document.getElementById("result").innerText=(
-                "If you deposit " + principle + ",\nat an interest rate of " + rate + ".\nYou will recieve an amount of " + interest + ",\nin the year " + year
-                );
-    }
-
-    if (principle > 0) {
+        document.getElementById("result").innerText=("If you deposit " + principle + ",\nat an interest rate of " + rate + "%.\nYou will recieve an amount of " + interest + ",\nin the year " + year);
+        }
+    if (principle <= 0) {
         alert("Enter a number greater than zero")
-    }
+        }
 }
 
 function updateRate() 
